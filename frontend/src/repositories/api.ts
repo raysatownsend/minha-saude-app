ffaa44e2ad34c090c9f0721138f593a9a290971f
raysatownsend <raysa.townsend@gmail.com>
2026-09-12 23:31:27 -0300

    ajustando URL como variavel de ambiente

--- frontend/src/repositories/api.ts
+++ frontend/src/repositories/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
+if (!apiUrl) {
+  throw new Error('EXPO_PUBLIC_API_URL não está definida — confira o .env');
+}
 
 export const api = axios.create({
-    baseURL: 'http://192.168.0.10: 3000',
+    baseURL: apiUrl,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
